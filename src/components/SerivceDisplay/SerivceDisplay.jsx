26cbce4e749194eb0673d85d6cff82fd801a702e
Bhargav <bhargavsai130@gmail.com>
2024-05-01 13:16:57 +0530

    first commit

--- src/components/SerivceDisplay/SerivceDisplay.jsx
+++ src/components/SerivceDisplay/SerivceDisplay.jsx
@@ -1,30 +1,4 @@
-// import React from 'react'
-// import './SerivceDisplay.css'
-// import { useContext } from 'react'
-// import { StoreContext } from '../../context/StoreContext'
-// import SerivceItem from '../SerivceItem/SerivceItem'
 
-// const SerivceDisplay = ({category}) => {
-
-//   const {serivce_women_list} = useContext(StoreContext)
-
-
-//   return (
-//     <div className='serivce-display' id='serivce-display'>
-//     <h2> Top Services near you</h2>
-//     <div className='serivce-display-list'>
-//      {serivce_women_list.map((item,index)=>{
-//       if(category==="All" || category === item.category){
-//       return <SerivceItem key={index} id={item._id} shop_name={item.shop_name} location={item.location} timing={item.timing} price={item.price} image={item.image}/>
-//       }
-//      })}
-//     </div>
-
-//     </div>
-//   )
-// }
-
-// export default SerivceDisplay
 import React, { useState } from 'react';
 import './SerivceDisplay.css';
 import { useContext } from 'react';
@@ -46,12 +20,20 @@ const SerivceDisplay = ({ category }) => {
     <div className='serivce-display' id='serivce-display'>
       <h2>Top Services near you</h2>
       <div className='search-inputs'>
-        <input
-          type='text'
-          placeholder='Enter Location'
-          value={searchLocation}
+        <select
+          id="location"
+          name="locationlist"
+          form="locationform"
           onChange={(e) => setSearchLocation(e.target.value)}
-        />
+          className='select'
+
+        >
+         <option value="Select Area">Select Area</option>
+          <option value="BTM Layout">BTM Layout</option>
+          <option value="Electronic city ">Electronic city </option>
+          <option value="J.P Nagar">J.P Nagar</option>
+          <option value="Koramangala">Koramangala</option>
+        </select>
         <input
           type='text'
           placeholder='Enter Shop Name'
